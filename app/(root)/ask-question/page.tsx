81b5442c03dd54553cbe35817bcc984c3cb56073
milan <milan.milancen12345@gmail.com>
2024-08-02 11:06:41 +0200

    removed monogo_id in app/ask-question/page.tsx

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -12,13 +12,13 @@ const Page = async () => {
 
   if (!userId) redirect("/sign-in");
 
-  const mongoUser = await getUserById({ userId: "1234567890" });
+  // const mongoUser = await getUserById({ userId: userId });
 
   return (
     <div>
       <h1 className="h1-bold text-dark100_light900">Ask a question</h1>
       <div className="mt-9">
-        <Question mongoUserId={JSON.stringify(mongoUser._id)} />
+        <Question mongoUserId={JSON.stringify(userId)} />
       </div>
     </div>
   );
